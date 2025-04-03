d9d0d6fc655984faf6e57a888790954a61c6222b
Maksim Lakovich <maks.lakovich@gmail.com>
2025-04-03 11:44:32 +0300

    1) Доработан сериализатор CourseSerializer() по выводу детальной информации всех уроков курса. 2) Обновлен README.md

--- lms_system/serializers.py
+++ lms_system/serializers.py
@@ -3,12 +3,23 @@ from rest_framework import serializers
 from lms_system.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    """Класс-сериализатор с использованием класса ModelSerializer для осуществления базовой сериализация в DRF на
+    основе модели Lesson. Описывает то, какие поля модели Lesson будут участвовать в сериализации и десериализации.
+    """
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"  # можно указать автоматом все поля модели
+
+
 class CourseSerializer(serializers.ModelSerializer):
     """Класс-сериализатор с использованием класса ModelSerializer для осуществления базовой сериализация в DRF на
     основе модели Course. Описывает то, какие поля модели Course будут участвовать в сериализации и десериализации.
     """
 
     count_lessons = serializers.SerializerMethodField()
+    lessons_info = LessonSerializer(many=True)
 
     def get_count_lessons(self, instance):
         """Функция для определения количества уроков в курсе. Запрос в БД для подсчёта связанных уроков."""
@@ -24,14 +35,5 @@ class CourseSerializer(serializers.ModelSerializer):
             "preview",
             "description",
             "count_lessons",
+            "lessons_info",
         ]  # можно указывать нужные поля модели
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    """Класс-сериализатор с использованием класса ModelSerializer для осуществления базовой сериализация в DRF на
-    основе модели Lesson. Описывает то, какие поля модели Lesson будут участвовать в сериализации и десериализации.
-    """
-
-    class Meta:
-        model = Lesson
-        fields = "__all__"  # можно указать автоматом все поля модели
